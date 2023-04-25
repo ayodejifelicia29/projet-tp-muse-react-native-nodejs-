537559ab154f2e5ee9f741504ee8d125052a015d
Felicia Ayodeji <felicia.ayodeji55@gmail.com>
2023-04-25 12:19:20 +0200

    accueil front

---
+++ projet-front/composants/Accueil.jsx
@@ -0,0 +1,16 @@
+import { Button, StyleSheet, Text, View ,Image} from 'react-native'
+import React from 'react'
+
+const Accueil = () => {
+  return (
+	<View>
+	  <Text>Accueil</Text>
+	  <Image/>
+	  <Button/>
+	</View>
+  )
+}
+
+export default Accueil
+
+const styles = StyleSheet.create({})
